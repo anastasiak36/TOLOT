2585aea398250a11cbcc3c3cd2f57433ba2f5bfd
anastasiak36 <akelnik.9@gmail.com>
2021-06-01 08:40:31 -0400

    create QuizGen file

---
+++ QuizGenerator.java
@@ -0,0 +1,5 @@
+public class QuizGenerator {
+    // have two lists, one for terms, one for definitions
+    // randomly choose what to ask the user(definitions) and then remove if correct
+    //keep track of score
+}
